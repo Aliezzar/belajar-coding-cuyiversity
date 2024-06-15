63e3a99e2e1151258e9c3ce2af82e256258da848
Aliezzar <105056102+Aliezzar@users.noreply.github.com>
2024-06-16 05:17:33 +0700

    tambahan sesi 6 semester 3

---
+++ cuy university semester 3/cuy university sesi 6 sem 3/index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const db = require('./connection')
+const response = require("./response")
+
+// routes/URL/Endpoint utama kita method nya GET
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    response(200, "API v1 ready to go", "SUCCESS", res)
+})
+
+app.get('/mahasiswa', (req, res) => {
+    const sql = "SELECT * FROM mahasiswa"
+    db.query(sql, (err, fields) => {
+        if (err) throw err
+        response(200, fields, "Mahasiswa Getlist", res)
+    })
+})
+
+app.get("/mahasiswa/:nim", (req, res) => {
+    const nim = req.params.nim
+    const sql = `SELECT * FROM mahasiswa WHERE nim = ${nim}`
+    db.query(sql, (err, fields) => {
+        if (err) throw err
+        response(200, fields, "get detail mahasiswa", res)
+    })
+})
+
+app.post("/mahasiswa", (req, res) => {
+    const { nim, namaLengkap, kelas, alamat } = req.body
+
+    const sql = `INSERT INTO mahasiswa (nim, nama_lengkap, kelas, alamat) VALUES (${nim}, '${namaLengkap}', '${kelas}', '${alamat}')`
+
+    db.query(sql, (err, fields) => {
+        if (err) response(500, "invalid", "error", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                id: fields.insertId,
+            }
+            response(200, data, "data added successfully", res)
+        }
+    })
+})
+
+app.put("/mahasiswa", (req, res) => {
+    const { nim, namaLengkap, kelas, alamat } = req.body
+    const sql = `UPDATE mahasiswa SET nama_lengkap = '${namaLengkap}', kelas = '${kelas}', alamat = '${alamat}' WHERE nim = ${nim}`
+    db.query(sql, (err, fields) => {
+        if (err) response(500, "invalid", "error", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                message: fields.message,
+            }
+            response(200, data, "Update data successfully", res)
+        } else {
+            response(404, "user not found", "error", res)
+        }
+    })
+
+})
+
+app.delete("/mahasiswa", (req, res) => {
+    const { nim } = req.body
+    const sql = `DELETE FROM mahasiswa WHERE nim = ${nim}`
+    db.query(sql, (err, fields) => {
+        if (err) response(500, "invalid", "error", res)
+        console.log(fields)
+    })
+    response(200, "delete test", "INI DELETE DATA", res)
+})
+
+app.listen(port, () => {
+    console.log(`Example app lisrening on port ${port}`)
+})
